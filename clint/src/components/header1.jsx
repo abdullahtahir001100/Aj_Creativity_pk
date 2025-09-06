f6b4a9e7f7b840356bf62fb7fc7ff12314c58893
abdullahtahir001100 <abdullahtahi001@gmail.com>
2025-09-06 14:03:15 +0500

    add

--- clint/src/components/header1.jsx
+++ clint/src/components/header1.jsx
@@ -29,56 +29,61 @@ const Header = () => {
 
   return (
     <header>
-        <div className="flexbox">
-            <nav className="flexbox">
-              <ul>
-                <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
-                <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link></li>
-                <li><Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>Contact</Link></li>
-              </ul>
-                        <div className="logo">
+      <div className="mobilehide">
+        <nav className="flexbox">
+          <ul>
+            <li><Link to="" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
+            <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link></li>
+            <li><Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>Contact</Link></li>
+          </ul>
+          <div className="logo">
             <div className="col-1">
               <div className="image">
                 <Link to="/"><img src="./imgs - Copy/logo.svg" alt="Logo" /></Link>
               </div>
             </div>
           </div>
-              <ul>
-                {/* <li><Link to="/blogs" className={location.pathname === '/blogs' ? 'active' : ''}>Blogs</Link></li> */}
-                {/* Cart icon: hide on Cart page */}
-                <li><Link to="/product" className={`button${location.pathname === '/product' ? ' active' : ''}`}>Explore now</Link></li>
-                 {location.pathname !== '/cart' && (
-                  <li className='cart'>
-                    
-                    <Link to="/Cart" className={location.pathname === '/cart' ? 'active' : ''}>
-                      <img src="./shopping1.png" alt="" />
-                      {cartCount > 0 && <strong>{cartCount}</strong>}
-                    </Link>
-                  </li>
-                )}
-              </ul>
-              <a className="burger" onClick={openBurger}><img src="./burger.png" alt="Burger" /></a>
-            </nav>
-          
+          <ul>
+            {/* <li><Link to="/blogs" className={location.pathname === '/blogs' ? 'active' : ''}>Blogs</Link></li> */}
+            {/* Cart icon: hide on Cart page */}
+            <li><Link to="/product" className={`button${location.pathname === '/product' ? ' active' : ''}`}>Explore now</Link></li>
+            {location.pathname !== '/cart' && (
+              <li className='cart'>
+
+                <Link to="/Cart" className={location.pathname === '/cart' ? 'active' : ''}>
+                  <img src="./shopping1.png" alt="" />
+                  {cartCount > 0 && <strong>{cartCount}</strong>}
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+      <nav className='flexbox mobile'>
+        <div className="logo">
+          <div className="col-1">
+            <div className="image">
+              <Link to="/"><img src="./imgs - Copy/logo.svg" alt="Logo" /></Link>
+            </div>
+          </div>
         </div>
-       <div className="links" id="togal" style={{
-         top: burgerOpen ? '82px' : '-382px',
-         display: burgerOpen ? 'block' : 'none'
-       }}
-       >
-                 <li><Link to="/"><h6>Home</h6></Link></li>
-                 <li><Link to="/about"><h6>About</h6></Link></li>
-                 <li><Link to="/contact"><h6>Contact</h6></Link></li>
-                 {/* <li><Link to="/Blogs"><h6>Blogs</h6></Link></li> */}
-                 {/* Cart icon in burger menu, always show, but visually hide on Cart page */}
-                 <li className='cart' style={{ display: location.pathname === '/cart' ? 'none' : undefined }}>
-                   <Link to="/Cart" className={location.pathname === '/cart' ? 'active' : ''}>
-                    <h5>Cart</h5>
-                   </Link>
-                 </li>
-                 <li><Link to="/product" className="buttn"><h4>Explore now</h4></Link></li>
-               </div>
-     
+        <div className="burgerbtn">  <a className="burger" onClick={openBurger}><img src="./burger.png" alt="Burger" /></a></div>
+      </nav>
+      <div className="links" id="togal" style={{
+        top: burgerOpen ? '157px' : '-382px',
+        display: burgerOpen ? 'block' : 'none'
+       }}>
+        <li><Link to="/"><h6>Home</h6></Link></li>
+        <li><Link to="/about"><h6>About</h6></Link></li>
+        <li><Link to="/contact"><h6>Contact</h6></Link></li>
+        <li>
+          <Link to="/Cart" className={location.pathname === '/cart' ? 'active' : ''}>
+            <h5>Cart</h5>
+          </Link>
+        </li>
+        <li><Link to="/product" className="buttn"><h4>Explore now</h4></Link></li>
+      </div>
+
     </header>
   );
 };
